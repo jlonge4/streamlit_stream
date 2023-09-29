8c0630a941098e071fed240459ef593784cb39fa
jlonge4 <91354480+jlonge4@users.noreply.github.com>
2023-09-28 20:19:36 -0400

    Create openai.py

---
+++ openai.py
@@ -0,0 +1,24 @@
+import openai
+import time
+
+openai.api_key = ''
+
+def stream_openai(model, prompt, box):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            # {'role': 'user', 'content': "prompt"}
+            prompt
+        ],
+        temperature=0,
+        stream=True  
+    )
+    if response:
+        s = ''
+        for chunk in response:
+            if chunk['choices'][0]['finish_reason'] == "stop":
+                break
+            for c in chunk['choices'][0]['delta']['content']:
+                s += c
+                time.sleep(.002)
+                box.write(s)
